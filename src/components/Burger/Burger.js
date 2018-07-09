5425ebc2c8de73b2513ec7679e5afbf68182edc2
MD. Samsozzaman Palash <palashkhan.pk@gmail.com>
2018-07-09 23:13:38 +0600

    make burger with items dynamic from state object

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -2,12 +2,16 @@ import React from 'react';
 
 import Classes from './Burger.css';
 import BurgerIngredent from './BurgerIngredient/BurgerIngredient';
-const burger = () => {
+const burger = (props) => {
+    const transformIngredients = Object.keys(props.ingredients).map(igkey=>{
+        return [...Array(props.ingredients[igkey])].map((_,i)=>{
+            return <BurgerIngredent key={igkey+i} type={igkey} />
+        });
+    }); 
     return (
         <div className={Classes.Burger}>
             <BurgerIngredent type="bread-top" />
-            <BurgerIngredent type="cheese" />
-            <BurgerIngredent type="meat" />
+            {transformIngredients}
             <BurgerIngredent type="bread-bottom" />
         </div>
     );
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -3,10 +3,20 @@ import Oux from '../../hoc/Oux';
 import Burger from '../../components/Burger/Burger'
 
 class BurgerBuilder extends Component {
+
+    state = {
+        ingredients: {
+            salad: 1,
+            bacon: 1,
+            cheese:2,
+            meat: 2
+        }
+    }
+
     render () {
         return (
             <Oux>
-                <Burger />
+                <Burger ingredients={this.state.ingredients} />
                 <div> Build Controls </div>
             </Oux>
         );
